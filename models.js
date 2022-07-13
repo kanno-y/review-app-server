582e567cfe18e1a94e00679f52deca743ca80589
kanno-y <kanno0521@gmail.com>
2022-07-14 02:08:37 +0900

    update

--- models.js
+++ models.js
@@ -3,8 +3,7 @@ import Sequelize from "sequelize";
 const { DataTypes } = Sequelize;
 
 const url =
-  process.env.DATABASE_URL ||
-  "postgres://postgres:postgres@localhost:5432/review_app";
+  process.env.DATABASE_URL || "postgres://postgres@localhost:5432/review_app";
 export const sequelize = new Sequelize(url);
 
 export const User = sequelize.define(
